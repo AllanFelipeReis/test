df56f6a94b6c99df0522f6e3e29e81defb3da98b
Allan Reis <allanreis3006@gmail.com>
2024-12-01 09:08:13 -0300

    test

--- src/core/connection.py
+++ src/core/connection.py
@@ -1,13 +1,25 @@
-import mysql.connector
 import streamlit as st
+import pymysql
+# import mysql.connector
 
 # Conexão ao banco de dados
-
-
 def get_connection():
-    return mysql.connector.connect(
+    # return mysql.connector.connect(
+    #     host=st.secrets.db_credentials.host,
+    #     user=st.secrets.db_credentials.user,
+    #     password=st.secrets.db_credentials.password,
+    #     database=st.secrets.db_credentials.db
+    # )
+    timeout = 10
+    return pymysql.connect(
+        charset="utf8mb4",
+        connect_timeout=timeout,
+        cursorclass=pymysql.cursors.DictCursor,
+        db=st.secrets.db_credentials.db,
         host=st.secrets.db_credentials.host,
-        user=st.secrets.db_credentials.user,
         password=st.secrets.db_credentials.password,
-        database=st.secrets.db_credentials.database
+        read_timeout=timeout,
+        port=16202,
+        user=st.secrets.db_credentials.user,
+        write_timeout=timeout,
     )
